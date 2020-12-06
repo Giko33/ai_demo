d32385ae3e919bedd529d525892352bdff405c7b
Sayan Datta <sayandatta33@gmail.com>
2020-12-06 17:05:47 +0530

    initial commit

---
+++ static/js/main.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+    // Init
+    console.log("initialized");
+    // $('.image-section').hide();
+    // // $('.loader').hide();
+    // $('#result').hide();
+
+    // Upload Preview
+    const readURL=(input)=> {
+        console.log("read",input);
+        if (input.files && input.files[0]) {
+            console.log("in if");
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
+                $('#imagePreview').fadeIn(650);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#imageUpload").change(()=> {
+        console.log("triggered");
+        $('.image-section').show();
+        $('#btn-predict').show();
+        $('#result').text('');
+        // $('#result').hide();
+        console.log("printing");
+        console.log(readURL);
+        readURL(document.getElementById("imageUpload"));
+    });
+
+    // Predict
+    $('#btn-predict').click(function () {
+        var form_data = new FormData($('#upload-file')[0]);
+
+        // Show loading animation
+        // $(this).hide();
+        $('.loader').show();
+
+        // Make prediction by calling api /predict
+        $.ajax({
+            type: 'POST',
+            url: '/predict',
+            data: form_data,
+            contentType: false,
+            cache: false,
+            processData: false,
+            async: true,
+            success: function (data) {
+                // Get and display the result
+                // $('.loader').hide();
+                $('#result').fadeIn(600);
+                $('#result').text(' Result:  ' + data);
+                console.log('Success!');
+            },
+        });
+    });
+
+});
